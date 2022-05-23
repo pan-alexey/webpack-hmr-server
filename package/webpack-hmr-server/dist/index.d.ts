5a807832ec019355c3aa4f170b6ae446928408ff
Aleksey Pan <agpan@avito.ru>
2022-05-23 19:10:29 +0300

    fix export

--- package/webpack-hmr-server/dist/index.d.ts
+++ package/webpack-hmr-server/dist/index.d.ts
@@ -2,17 +2,17 @@ import webpack from 'webpack';
 import * as http from 'http';
 
 declare class HotModuleServer {
-    private socketServer;
-    private putStats;
-    constructor(server: http.Server);
-    private listenClinets;
-    private sendBroadcast;
-    private getModuleData;
-    processStats(stats?: webpack.Stats | null): void;
-    processReload(): void;
+  private socketServer;
+  private putStats;
+  constructor(server: http.Server);
+  private listenClinets;
+  private sendBroadcast;
+  private getModuleData;
+  processStats(stats?: webpack.Stats | null): void;
+  processReload(): void;
 }
 
 declare const manual: (serve: http.Server) => HotModuleServer;
 declare const _default: (compiler: webpack.Compiler, serve: http.Server) => void;
 
-export { _default as default, manual };
+export { HotModuleServer, _default as default, manual };
